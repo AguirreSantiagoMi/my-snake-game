11f122f5bd1c3c10b371098537fd7f7a589ea0cb
Aguirre Santiago <santiago.aguirre.mi@gmail.com>
2024-08-27 17:29:21 -0300

    Game over bug check

--- main.py
+++ main.py
@@ -36,20 +36,22 @@ while game_is_on:
     
     #Detect collision with wall
     if snake.head.xcor() > 270 or snake.head.xcor() < -290 or snake.head.ycor() > 290 or snake.head.ycor() < -270:
-        game_is_on = screen.textinput('GAME OVER', 'Play again? y/n: ').lower()
-        if game_is_on == 'y':
+        game_is_on = screen.textinput('GAME OVER', 'Play again? y/n: ')
+        if game_is_on == 'y' or game_is_on == 'Y':
             pass
-        if game_is_on == 'n':
+        if game_is_on == 'n' or game_is_on == 'N':
             game_is_on = False
-        score.reset()
-        snake.reset()
+        else:
+            score.reset()
+            snake.reset()
     #Detect collision with tail
     for segment in snake.segments[1:]:
         if snake.head.distance(segment) < 10:
             game_is_on = screen.textinput('GAME OVER', 'Play again? y/n: ').lower()
-            if game_is_on == 'y':
+            if game_is_on == 'y' or game_is_on == 'Y':
                 pass
-            if game_is_on == 'n':
+            if game_is_on == 'n' or game_is_on == 'N':
                 game_is_on = False
-            score.reset()
-            snake.reset()
+            else:
+                score.reset()
+                snake.reset()
