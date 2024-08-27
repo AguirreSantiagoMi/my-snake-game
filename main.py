b41777aae46418d059529bb90678645f292d7027
Aguirre Santiago <santiago.aguirre.mi@gmail.com>
2024-08-27 16:07:56 -0300

    Game over

--- main.py
+++ main.py
@@ -14,15 +14,16 @@ food = Food()
 score = Scoreboard()
 screen.update()
 
-screen.listen()
-screen.onkey(snake.up, "Up")
-screen.onkey(snake.down, "Down")
-screen.onkey(snake.left, "Left")
-screen.onkey(snake.right, "Right")
+
 
 game_is_on = True
 theres_food = False
 while game_is_on:
+    screen.listen()
+    screen.onkey(snake.up, "Up")
+    screen.onkey(snake.down, "Down")
+    screen.onkey(snake.left, "Left")
+    screen.onkey(snake.right, "Right")
     screen.update()
     snake.move()
     screen.update()
@@ -35,12 +36,20 @@ while game_is_on:
     
     #Detect collision with wall
     if snake.head.xcor() > 270 or snake.head.xcor() < -290 or snake.head.ycor() > 290 or snake.head.ycor() < -270:
+        game_is_on = screen.textinput('GAME OVER', 'Play again? y/n: ').lower()
+        if game_is_on == 'y':
+            pass
+        if game_is_on == 'n':
+            game_is_on = False
         score.reset()
         snake.reset()
     #Detect collision with tail
     for segment in snake.segments[1:]:
         if snake.head.distance(segment) < 10:
+            game_is_on = screen.textinput('GAME OVER', 'Play again? y/n: ').lower()
+            if game_is_on == 'y':
+                pass
+            if game_is_on == 'n':
+                game_is_on = False
             score.reset()
             snake.reset()
-
-screen.exitonclick()
